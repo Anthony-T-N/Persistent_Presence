a8ca154fcd26d7f6457674c3f17ddf5bdf2dac50
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2021-01-09 14:55:12 +1030

    Update Persistent_Presence.cs

--- Persistent_Presence.cs
+++ Persistent_Presence.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Diagnostics;
 
 namespace Persistent_Presence
 {
     class Persistent_Presence
     {
-
+        public bool check_executable_placement()
+        {
+            if (!System.IO.Directory.Exists(""))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }    
+        }
+        public void duplicate_executables()
+        {
+            // Create folder in "Program Files" and store executable.
+            string current_path_location = @"C:\Users\Anthony\source\repos\Persistent_Presence\bin\Debug\netcoreapp3.1\temp";
+            if (!System.IO.Directory.Exists(current_path_location))
+            {
+                System.IO.Directory.CreateDirectory(current_path_location);
+                Debug.WriteLine("[+] Creating \"{0}\"", current_path_location);
+            }
+        }
     }
 }
