2f371742003ef9f090ebbcd95f05369f7f0239f5
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2021-01-11 13:58:52 +1030

    Update Persistent_Presence.cs

--- Persistent_Presence.cs
+++ Persistent_Presence.cs
@@ -6,36 +6,29 @@ namespace Persistent_Presence
 {
     class Persistent_Presence
     {
-        public bool check_executable_placement()
-        {
-            if (!System.IO.Directory.Exists(""))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }    
-        }
+        private string primary_path_location = @"C:\Users\Anthony\source\repos\Persistent_Presence\bin\Debug\netcoreapp3.1\temp";
+        private string seconday_path_location = @"C:\Users\Anthony\Desktop\temp";
+        private string exe_location = Directory.GetCurrentDirectory() + @"\Persistent_Presence.exe";
+
         public void check_executable_location()
         {
             // Create folder in "Program Files" and store executable.
-            string primary_path_location = @"C:\Users\Anthony\source\repos\Persistent_Presence\bin\Debug\netcoreapp3.1\temp";
             if (!System.IO.Directory.Exists(primary_path_location))
             {
                 System.IO.Directory.CreateDirectory(primary_path_location);
                 Debug.WriteLine("[+] Creating \"{0}\"", primary_path_location);
+                duplicate_executables(primary_path_location);
             }
-            string seconday_path_location = @"C:\Users\Anthony\Desktop\temp";
             if (!System.IO.Directory.Exists(seconday_path_location))
             {
                 System.IO.Directory.CreateDirectory(seconday_path_location);
                 Debug.WriteLine("[+] Creating \"{0}\"", seconday_path_location);
+                duplicate_executables(seconday_path_location);
             }
         }
-        public void duplicate_executables()
+        public void duplicate_executables(string path_location)
         {
-            File.Copy(source, dest);
+            File.Copy(exe_location, path_location + @"\Persistent_Presence.exe");
         }
     }
 }
