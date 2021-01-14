ea1386348a859ab7e12b9a5195b633352b65327b
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2021-01-14 14:18:19 +1030

    Update Persistent_Presence.cs

--- Persistent_Presence.cs
+++ Persistent_Presence.cs
@@ -7,7 +7,7 @@ namespace Persistent_Presence
     class Persistent_Presence
     {
         private string primary_path_location = @"C:\Users\Anthony\source\repos\Persistent_Presence\bin\Debug\netcoreapp3.1\temp";
-        private string seconday_path_location = @"C:\Users\Anthony\Desktop\temp";
+        private string seconday_path_location = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\temp";
         private string exe_location = Directory.GetCurrentDirectory() + @"\Persistent_Presence.exe";
 
         public void check_executable_location()
@@ -16,22 +16,22 @@ namespace Persistent_Presence
             if (!System.IO.Directory.Exists(primary_path_location))
             {
                 System.IO.Directory.CreateDirectory(primary_path_location);
-                Debug.WriteLine("[+] Creating \"{0}\"", primary_path_location);
+                Debug.WriteLine(string.Format("[+] Creating {0}", primary_path_location));
                 duplicate_executables(primary_path_location);
             }
             else
             {
-                Debug.WriteLine("[=] {0} already exist.", primary_path_location);
+                Debug.WriteLine(string.Format("[=] {0} already exist.", primary_path_location));
             }
             if (!System.IO.Directory.Exists(seconday_path_location))
             {
                 System.IO.Directory.CreateDirectory(seconday_path_location);
-                Debug.WriteLine("[+] Creating \"{0}\"", seconday_path_location);
+                Debug.WriteLine(string.Format("[+] Creating {0}", seconday_path_location));
                 duplicate_executables(seconday_path_location);
             }
             else
             {
-                Debug.WriteLine("[=] {0} already exist.", seconday_path_location);
+                Debug.WriteLine(string.Format("[=] {0} already exist.", seconday_path_location));
             }
         }
         public void duplicate_executables(string path_location)
